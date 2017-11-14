a69aa7726a1de48ddc4ad88e90be9b9dc107023c
gpasupathy <gunasekaran.pasupathy@sony.com>
2017-11-14 07:46:42 -0800

    Class renamed

--- src/main/java/com/popcorn/api/moviesdb/logic/TrendingService.java
+++ src/main/java/com/popcorn/api/moviesdb/logic/TrendingService.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-public class TrendingServiceLogic {
+public class TrendingService {
 
     public static List<Trend> getTrends(){
         String URI = "https://api.themoviedb.org/3/discover/movie?api_key=6d3b69e7a8589ed8558a414d661e7ee1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2017";
